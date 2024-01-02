efc3667eb5b0451d1fca3037f183de7dfcedea14
NOOB ZERO <121302058+vbbdv3@users.noreply.github.com>
2024-01-02 16:04:43 +0500

    Update raidtp.lua

--- raidtp.lua
+++ raidtp.lua
@@ -3,7 +3,7 @@ while not game:IsLoaded() do
 end
 local libary = loadstring(game:HttpGet("https://pastebin.com/raw/RvJ0qewm", true))()
 local main = libary:CreateWindow("RAIDS")
-local mod = main:Button("RAID TP", function()
+local mod = main:Button("START", function()
 local args = {
     [1] = {
         [1] = {
@@ -15,5 +15,52 @@ local args = {
 }
 
 game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
+wait(0.5)
+local args = {
+    [1] = {
+        [1] = {
+            [1] = "\3",
+            [2] = "Raid",
+            [3] = "Join"
+        }
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
+wait(0.5)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(2559.81884765625, 2832.14013671875, 7060.63623046875)
+wait(0.5)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(2510.676513671875, 2832.141845703125, 7115.962890625)
+wait(0.5)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(2562.3486328125, 2832.142333984375, 7178.28662109375)
+wait(0.5)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(2608.69580078125, 2832.141357421875, 7116.05029296875)
+wait(0.5)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(2564.9228515625, 2832.14208984375, 7121.92724609375)
+wait(0.5)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(2559.81884765625, 2832.14013671875, 7060.63623046875)
+wait(0.5)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(2510.676513671875, 2832.141845703125, 7115.962890625)
+wait(0.5)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(2562.3486328125, 2832.142333984375, 7178.28662109375)
+wait(0.5)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(2608.69580078125, 2832.141357421875, 7116.05029296875)
+wait(0.5)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(2564.9228515625, 2832.14208984375, 7121.92724609375)
+wait(0.5)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(2559.81884765625, 2832.14013671875, 7060.63623046875)
+wait(0.5)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(2510.676513671875, 2832.141845703125, 7115.962890625)
+wait(0.5)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(2562.3486328125, 2832.142333984375, 7178.28662109375)
+wait(0.5)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(2608.69580078125, 2832.141357421875, 7116.05029296875)
+wait(0.5)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(2564.9228515625, 2832.14208984375, 7121.92724609375)
+wait(0.5)
+end)
+local mod = main:Button("NEXT SERVER", function()
 end)
 
