280b4dbff8faaae407f89c526e087838b2cc8254
NOOB ZERO <121302058+vbbdv3@users.noreply.github.com>
2024-01-09 03:01:57 +0500

    Update punch.lua

--- punch.lua
+++ punch.lua
@@ -1,1 +1,27 @@
 
+while not game:IsLoaded() do
+    wait()
+end
+local libary = loadstring(game:HttpGet("https://pastebin.com/raw/RvJ0qewm", true))()
+local main = libary:CreateWindow("KILL FARM")
+
+local mod = main:Button("START", function()
+--while true do wait()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(9549.4228515625, 18.674135208129883, -332.7200012207031)
+wait(0.33)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(9564.814453125, 18.674135208129883, -352.1691589355469)
+wait(0.33)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(9531.4736328125, 18.674135208129883, -350.13311767578125)
+wait(0.33)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(9532.2568359375, 18.674135208129883, -313.1447448730469)
+wait(0.33)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(9560.2490234375, 18.674135208129883, -315.2872314453125)
+wait(0.33)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(9508.3173828125, 18.467288970947266, -330.878662109375)
+wait(0.33)
+--end
+  end)
+
+local mod = main:Button("STOP", function()
+game.Players.LocalPlayer.Character.Torso:Destroy()
+end)
