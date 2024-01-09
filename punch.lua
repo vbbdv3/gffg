a19cb8ed0f50113147c5eb214f64b211ac316db6
NOOB ZERO <121302058+vbbdv3@users.noreply.github.com>
2024-01-09 19:04:02 +0500

    Update punch.lua

--- punch.lua
+++ punch.lua
@@ -27,27 +27,6 @@ wait(0.6)
 end
   end)
 
-local mod = main:Button("5 MINUTE MOVE", function()
-asd = true
-spawn(function()
-    task.wait(300)
-    asd = false
-end)
-while asd do
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(9549.4228515625, 18.674135208129883, -332.7200012207031)
-wait(0.6)
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(9564.814453125, 18.674135208129883, -352.1691589355469)
-wait(0.6)
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(9531.4736328125, 18.674135208129883, -350.13311767578125)
-wait(0.6)
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(9532.2568359375, 18.674135208129883, -313.1447448730469)
-wait(0.6)
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(9560.2490234375, 18.674135208129883, -315.2872314453125)
-wait(0.6)
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(9508.3173828125, 18.467288970947266, -330.878662109375)
-wait(0.6)
-end
-  end)
 
 local mod = main:Button("JOIN", function()
 local args = {
