66fe81bd13126f35e522b74b1d4054e6dcb3da4d
NOOB ZERO <121302058+vbbdv3@users.noreply.github.com>
2024-01-09 19:10:51 +0500

    Update punch.lua

--- punch.lua
+++ punch.lua
@@ -78,10 +78,10 @@ wait(0.6)
 end
 end)
 
-local mod = main:Button("5 MINUTE TP", function()
+local mod = main:Button("3 MINUTE TP", function()
 asd = true
 spawn(function()
-    task.wait(300)
+    task.wait(90)
     asd = false
 end)
 while asd do
