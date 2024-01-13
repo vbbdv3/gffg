5525d42f6338d4ae025bbc47395e0b654f7e4fca
NOOB ZERO <121302058+vbbdv3@users.noreply.github.com>
2024-01-13 17:38:03 +0500

    Create grim.lua

---
+++ grim.lua
@@ -0,0 +1,28 @@
+
+while not game:IsLoaded() do
+    wait()
+end
+local libary = loadstring(game:HttpGet("https://pastebin.com/raw/RvJ0qewm", true))()
+local main = libary:CreateWindow("RAID")
+
+local mod = main:Button("GRIM", function()
+asd = true
+spawn(function()
+    task.wait(300)
+    asd = false
+end)
+while asd do
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-3892.120849609375, 20.230531692504883, -424.9251708984375)
+wait(0.6)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-3892.50830078125, 20.230533599853516, -455.0065002441406)
+wait(0.6)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-3866.062744140625, 13.087101936340332, -479.2889404296875)
+wait(0.6)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-3862.10791015625, 13.087101936340332, -543.6198120117188)
+wait(0.6)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-3830.763671875, 13.087101936340332, -530.2799072265625)
+wait(0.6)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-3825.801025390625, 13.087101936340332, -487.4297180175781)
+wait(0.6)
+end
+end)
