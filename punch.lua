fbaf212ce6b375005352df5e6b5b6e249ea4f3cf
NOOB ZERO <121302058+vbbdv3@users.noreply.github.com>
2024-01-14 11:07:08 +0500

    Update punch.lua

--- punch.lua
+++ punch.lua
@@ -6,16 +6,14 @@ local libary = loadstring(game:HttpGet("https://pastebin.com/raw/RvJ0qewm", true
 local main = libary:CreateWindow("RAID")
 
 local mod = main:Button("BOSS TP 5 minute", function()
-asd = true
-spawn(function()
-    task.wait(300)
-    asd = false
-end)
-while asd do
+getgenv().farmer = true
+while wait(1) do
+    if getgenv().farmer == true then
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(299.7926025390625, 35.33799743652344, -373.4683532714844)
 wait(0.6)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(290.1902160644531, 35.33799743652344, -305.388671875)
 wait(0.6)
+    end
 end
 end)
 
